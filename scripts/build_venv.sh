75b37701963838b17b5cf396d694d5273f6f0075
jamesbeedy <jamesbeedy@gmail.com>
2020-07-29 16:10:02 +0000

    bring the rest of the deps
    copy the needed include and library files into the charm venv

--- scripts/build_venv.sh
+++ scripts/build_venv.sh
@@ -9,6 +9,10 @@ docker run -it --rm \
     quay.io/pypa/manylinux2014_x86_64 \
     /bin/sh -c '/opt/python/cp38-cp38/bin/python -m venv --copies --clear /srv/build/venv && \
                 /opt/python/cp38-cp38/bin/pip install -r /srv/requirements.txt \
-                -t /srv/build/venv/lib/python3.8/site-packages && chown -R 1000:1000 /srv/build && \
+                -t /srv/build/venv/lib/python3.8/site-packages && \
+		cp -r /usr/local/lib/libcrypt.* /srv/build/venv/lib/ && \
+		cp -r /opt/_internal/cpython-3.8.5/lib/python3.8 /srv/build/venv/lib/ && \
+		cp -r /opt/_internal/cpython-3.8.5/include/* /srv/build/venv/include/ && \
+                chown -R 1000:1000 /srv/build && \
 		/opt/python/cp38-cp38/bin/python /srv/build/scripts/create_zip.py && \
 		chown -R 1000:1000 /srv/out'
